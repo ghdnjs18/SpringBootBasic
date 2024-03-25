2993cbaf4353f1c7889fa2821fb1d127b30c074c
hoho <ghdnjs18@naver.com>
2024-03-25 21:00:25 +0900

    자바로 주문 할인 테스트

---
+++ src/test/java/com/lecture/springbasic/order/OrderServiceTest.java
@@ -0,0 +1,34 @@
+package com.lecture.springbasic.order;
+
+import com.lecture.springbasic.discount.DiscountPolicy;
+import com.lecture.springbasic.discount.FixDiscountPolicy;
+import com.lecture.springbasic.member.Grade;
+import com.lecture.springbasic.member.Member;
+import com.lecture.springbasic.member.MemberService;
+import com.lecture.springbasic.member.memberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    private MemberService memberService = new memberServiceImpl();
+    private OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    @DisplayName("주문과 할인 정책 테스트")
+    void createOrder() {
+        // given
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        // when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        // then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
