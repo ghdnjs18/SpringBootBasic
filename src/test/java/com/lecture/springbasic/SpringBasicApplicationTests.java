15de6610ea0e9a81771608ef26d67f0ae15352d6
hoho <ghdnjs18@naver.com>
2024-03-25 17:48:07 +0900

    git init

---
+++ src/test/java/com/lecture/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
