de7ae653263d512e8428158fd37edf93c547c924
hoho <ghdnjs18@naver.com>
2024-03-25 18:08:20 +0900

    기본 회원 서비스 추가

---
+++ src/main/java/com/lecture/springbasic/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.lecture.springbasic.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
---
+++ src/main/java/com/lecture/springbasic/member/memberServiceImpl.java
@@ -0,0 +1,16 @@
+package com.lecture.springbasic.member;
+
+public class memberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
