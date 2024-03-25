661671d03039cbca48b1b66168d0e7d9c060a8d4
hoho <ghdnjs18@naver.com>
2024-03-25 18:15:17 +0900

    기본 회원 가입 테스트

---
+++ src/main/java/com/lecture/springbasic/MemberApp.java
@@ -0,0 +1,19 @@
+package com.lecture.springbasic;
+
+import com.lecture.springbasic.member.Grade;
+import com.lecture.springbasic.member.Member;
+import com.lecture.springbasic.member.MemberService;
+import com.lecture.springbasic.member.memberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new memberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("find member = " + findMember.getName());
+    }
+}
