8df114e9a6703f4913910de5532260678350e16a
hoho <ghdnjs18@naver.com>
2024-03-26 15:43:11 +0900

    자바로 AppConfig 스프링 기반으로 변경

--- src/main/java/com/lecture/springbasic/AppConfig.java
+++ src/main/java/com/lecture/springbasic/AppConfig.java
@@ -9,25 +9,32 @@ import com.lecture.springbasic.member.MemoryMemberRepository;
 import com.lecture.springbasic.member.memberServiceImpl;
 import com.lecture.springbasic.order.OrderService;
 import com.lecture.springbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // 애플리케이션의 전체 동작 방식을 구성하기 위해, 구현 객체를 생성하고,
 // 연결하는 책임을 가지는 별도의 설정 클래스가 등장한다.
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new memberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     // 구성 정보에서 역할과 구현을 명확하게 분리해 역할이 잘 드러나고 중복을 제거함
-    private static MemberRepository memberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private static DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
