2104f02291499e07dbebc0d2a124ddb8cb37ae12
hoho <ghdnjs18@naver.com>
2024-03-27 18:40:00 +0900

    빈 샐명주기 콜백 테스트

--- src/test/java/com/lecture/springbasic/lifecycle/NetworkClient.java
+++ src/test/java/com/lecture/springbasic/lifecycle/NetworkClient.java
@@ -1,5 +1,11 @@
 package com.lecture.springbasic.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+//public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
 
     private String url;
@@ -14,15 +20,43 @@ public class NetworkClient {
         this.url = url;
     }
 
-    private void call(String message) {
+    public void call(String message) {
         System.out.println("call : " + url + ", message : " + message);
     }
 
-    private void connect() {
+    public void connect() {
         System.out.println("connect : " + url);
     }
 
     public void dissonnect() {
         System.out.println("close : " + url);
+    }
+
+    // InitializingBean에서 메서드로 초기화를 지원한다.
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+
+    // DisposableBean에서 메서드로 소멸을 지원한다.
+//    @Override
+//    public void destroy() throws Exception {
+//        System.out.println("NetworkClient.destroy");
+//        dissonnect();
+//    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        dissonnect();
     }
 }
