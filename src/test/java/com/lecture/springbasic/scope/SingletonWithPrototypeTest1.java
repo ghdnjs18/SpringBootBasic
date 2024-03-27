54f3ced00b878c5996b1bb66a51cee74c0879231
hoho <ghdnjs18@naver.com>
2024-03-27 21:06:35 +0900

    싱글톤 빈과 프로토타입 빈 함께 사용시 문제 해결

--- src/test/java/com/lecture/springbasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/lecture/springbasic/scope/SingletonWithPrototypeTest1.java
@@ -4,9 +4,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -36,16 +40,52 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count2).isEqualTo(2);
     }
 
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(1);
+    }
+
+    // 싱글톤 빈에서 프로토타입 빈 사용
+//    @Scope("singleton")
+//    static class ClientBean {
+//        private final PrototypeBean prototypeBean;
+//
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
+//
+//        public int logic() {
+//            prototypeBean.addCount();
+//            int count = prototypeBean.getCount();
+//            return count;
+//        }
+//    }
+
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
+        // 스프링의 애플리케이션 컨텍스트 전체를 주입받게 되면, 스프링 컨테이너에 종속적인 코드가 되고,
+        // 단위 테스트도 어려워진다.
+//        @Autowired
+//        private ApplicationContext ac;
+
+        // 의존관계 조회 DL
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        private ObjectProvider<PrototypeBean> protorypeBeanProvider;
+        // JSR-330 Provider 사용
+        private Provider<PrototypeBean> protorypeBeanProvider;
 
         public int logic() {
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = protorypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
